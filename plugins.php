73d2afa9ed0fbd687650240118f815583bf1d8d1
Raymond Chong <mathsgod@yahoo.com>
2021-01-07 10:41:01 +0800

    init

---
+++ plugins.php
@@ -0,0 +1,15 @@
+<?php
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+return function ($context, $inject) {
+
+    $config = $this->config["log"];
+    $log = new Logger($config["name"]);
+    foreach ($config["handler"] as $handler) {
+        $log->pushHandler(new StreamHandler($handler["stream"], $handler["level"] ?? Logger::DEBUG));
+    }
+
+    $inject("log", $log);
+};
